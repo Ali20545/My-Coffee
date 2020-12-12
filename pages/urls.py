408176ce1a37bc7e41f7f91aa23c139c32ac05d4
Ali919-hue <kly54545454@gmail.com>
2020-12-12 16:02:49 +0300

    new

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,10 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
     path('coffee', views.coffee, name='coffee'),
+    path('signin', views.signin, name='signin'),
+    path('signup', views.signup, name='signup'),
+    path('profile', views.profile, name='profile'),
+    path('search', views.search, name='search'),
+    path('products', views.products, name='products'),
+    path('product', views.product, name='product'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -12,3 +12,21 @@ def about(request):
 
 def coffee(request):
     return render(request, 'pages/coffee.html')
+
+def signin(request):
+    return render(request, 'pages/signin.html')
+
+def signup(request):
+    return render(request, 'pages/signup.html')
+
+def profile(request):
+    return render(request, 'pages/profile.html')
+
+def search(request):
+    return render(request, 'pages/search.html')
+
+def products(request):
+    return render(request, 'pages/products.html')
+
+def product(request):
+    return render(request, 'pages/product.html')
