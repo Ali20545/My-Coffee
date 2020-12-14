6d6c9b8c16a61651c9bae7682817418f3473397f
Ali919-hue <kly54545454@gmail.com>
2020-12-14 19:53:27 +0300

    new of to day

--- Mycoffee/settings.py
+++ Mycoffee/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Mycoffee.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'My-Coffee',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
