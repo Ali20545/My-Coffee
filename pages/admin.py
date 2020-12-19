9d10004703e321d6bbdce799ca207ab0791e9ec4
Ali919-hue <kly54545454@gmail.com>
2020-12-19 16:38:44 +0300

    admin display

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import Coffee
+from django.utils.html import format_html
 # Register your models here.
 
-admin.site.register(Coffee)
+class Coffeeadmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius: 10px;" />'.format(object.photo.url))
+
+    thumbnail.short_description = 'Photo'
+
+    list_display = ('id', 'thumbnail', 'Product_Name', 'Description', 'create_date')
+    list_display_links = ('id', 'thumbnail', 'Product_Name',)
+    search_fields = ('Product_Name', 'Description',)
+    list_filter = ('Product_Name','Description',)
+    
+
+
+admin.site.register(Coffee, Coffeeadmin)
